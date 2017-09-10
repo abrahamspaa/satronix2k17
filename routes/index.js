d6839feb9256340c35eb7e1dcec0d290c21c6d6a
Paul Abraham <abrahamspaa@gmail.com>
2017-09-10 20:02:13 +0100

    First draft

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ALPHA COLLEGE OF ENGINEERING' });
+  res.render('index', { title: 'ALPHA COLLEGE OF ENGINEERING', eventname: 'SATRONIX2K17' });
 });
 
 module.exports = router;
